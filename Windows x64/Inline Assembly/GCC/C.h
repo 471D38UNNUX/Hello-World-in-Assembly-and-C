08f12f1cf7e552cc7b64ef37a133fdaa748511bb
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2024-05-25 02:58:24 +0700

    Create C.h

---
+++ Windows x64/Inline Assembly/GCC/C.h
@@ -0,0 +1,13 @@
+void Hello(char msg[])
+{
+    asm volatile
+    (
+        "mov %0, %%rcx\n"
+        "call printf\n"
+        
+        "ret\n"
+        :
+        : "r"(msg)
+        : "%rax", "%rcx"
+    );
+}
