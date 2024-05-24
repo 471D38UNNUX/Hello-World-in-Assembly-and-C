026f1b651f0a9de8121eceab148445f5ff2b9ad8
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2024-05-25 02:10:20 +0700

    Create ABI.h

---
+++ Windows x64/Inline Assembly/GCC/ABI.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+const char * Hello(char msg[], size_t len)
+{
+    asm volatile
+    (
+        "sub $40, %%rsp\n"
+        
+        "mov $-11, %%rcx\n"
+        "call GetStdHandle\n"
+        "mov %%rax, %%rcx\n"
+        "mov %0, %%rdx\n"
+        "mov %1, %%r8\n"
+        "xor %%r9, %%r9\n"
+        "movq $0, 32(%%rsp)\n"
+        "call WriteConsoleA\n"
+
+        "add $40, %%rsp\n"
+
+        "xor %%rcx, %%rcx\n"
+        "call ExitProcess\n"
+        :
+        : "r"(msg), "r"(len)
+        : "%rax", "%rcx", "%rdx", "%r8", "%r9", "memory"
+    );
+    return 0;
+}
