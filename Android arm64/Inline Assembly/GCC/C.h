8fcd0168b4df06d328520b47f7387c9bab49f285
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2024-05-27 00:35:02 +0700

    Create C.h

---
+++ Android arm64/Inline Assembly/GCC/C.h
@@ -0,0 +1,15 @@
+void Hello(char msg[])
+{
+    asm volatile
+    (
+        "stp x29, x30, [sp, -16]!\n"
+        "mov x29, sp\n"
+        "mov x0, %0\n"
+        "bl printf\n"
+        "ldp x29, x30, [sp], 16\n"
+        "ret\n"
+        :
+        : "r"(msg)
+        : "x0", "x29", "x30"
+    );
+}
